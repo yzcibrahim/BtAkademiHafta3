0fac12e44c4bb4a141443c8b31d803cae7b59904
ibrahim <yzcibrahim@gmail.com>
2022-03-27 14:13:35 +0300

    son.

--- 04_KisiTakip/Program.cs
+++ 04_KisiTakip/Program.cs
@@ -1,6 +1,8 @@
 ﻿using _05_Tipler;
+using _07_CommonOps;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace _04_KisiTakip
@@ -10,56 +12,82 @@ namespace _04_KisiTakip
         
         static void Main(string[] args)
         {
-            
+           
             List<KisiCls> kisiler = new List<KisiCls>();
             while (true)
             {
-                Console.WriteLine("Yeni Kişi Eklemek için E, yazdırmak için Y, Kişi silmek için S giriniz");
+                Console.WriteLine("Yeni Kişi Eklemek için E, yazdırmak için Y, filtreleme yapamk için F, Kişi silmek için S giriniz");
                 string islem = Console.ReadLine();
                 if (islem.ToUpper() == "E")
                 {
                     KisiEkle(kisiler);
                 }
-                else if(islem=="Y")
+                else if (islem == "Y")
                 {
                     KisileriYazdir(kisiler);
                 }
-                else if(islem=="S")
+                else if (islem == "S")
                 {
                     KisiSil(kisiler);
+                }
+                else if (islem == "F")
+                {
+                    Filtrele(kisiler);
                 }
             }
 
         }
 
+        static void Filtrele(List<KisiCls> liste)
+        {
+            Console.WriteLine("Aranacak kelimeyi giriniz:");
+            string aranacakKelime = Console.ReadLine();
+            List<KisiCls> filtrelenmisListe = new List<KisiCls>();
+            filtrelenmisListe = liste.Where(c => c.Ad == aranacakKelime || c.Soyad==aranacakKelime).ToList();
+            //foreach (var item in liste)
+            //{
+            //    if (item.Ad == aranacakKelime || item.Soyad == aranacakKelime)
+            //    {
+            //        filtrelenmisListe.Add(item);
+            //    }
+            //}
+
+            KisileriYazdir(filtrelenmisListe);
+
+        }
+
         static void KisiSil(List<KisiCls> pListe)
         {
-            Console.WriteLine("Silinecek id giriniz:");
-            int silinecekId = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Silinecek id giriniz:");
+            //int silinecekId = Convert.ToInt32(Console.ReadLine());
+            int silinecekId = Operations.IntOku("Silinecek Id giriniz");
             KisiCls silinecekKisi = new KisiCls();
-            foreach (KisiCls kisi in pListe)
-            {
-                if (kisi.Id == silinecekId)
-                {
-                    Console.WriteLine($"{kisi.Ad} {kisi.Soyad} kişisi silinecek emin misiniz E/H?");
-                    string secim = Console.ReadLine();
-                    if (secim == "E")
-                    {
-                        silinecekKisi = kisi;
-                        break;
-                    }
-                    else
-                        return;
 
-                }
-            }
+            // silinecekKisi = pListe.Where(c => c.Id == silinecekId).First();
+            silinecekKisi = pListe.First(x=>x.Id== silinecekId);
+            //foreach (KisiCls kisi in pListe)
+            //{
+            //    if (kisi.Id == silinecekId)
+            //    {
+            //        Console.WriteLine($"{kisi.Ad} {kisi.Soyad} kişisi silinecek emin misiniz E/H?");
+            //        string secim = Console.ReadLine();
+            //        if (secim == "E")
+            //        {
+            //            silinecekKisi = kisi;
+            //            break;
+            //        }
+            //        else
+            //            return;
+
+            //    }
+            //}
 
             pListe.Remove(silinecekKisi);
 
         }
         static void KisileriYazdir(List<KisiCls> liste)
         {
-            foreach (KisiCls item in liste)
+            foreach (var item in liste)
             { //break;continue
               //   Console.WriteLine($"{item.Id} {item.Ad} {item.Soyad} {item.Yas} {item.Cinsiyet}");
                 Console.WriteLine(item.Yazdir());
@@ -81,8 +109,9 @@ namespace _04_KisiTakip
             Console.WriteLine("Kişinin soyadını giriniz:");
             eklenecekKisi.Soyad = Console.ReadLine();
 
-            Console.WriteLine("Kişinin yaşını giriniz:");
-            eklenecekKisi.Yas = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Kişinin yaşını giriniz:");
+            //eklenecekKisi.Yas = Convert.ToInt32(Console.ReadLine());
+            eklenecekKisi.Yas = Operations.IntOku("Kişinin yaşını giriniz:");
 
             Console.WriteLine("Erkek için E Kadın için K belirtmek ismeyiyorsa B giriniz:");
             string cinsiyetStr = Console.ReadLine();
---
+++ 07_CommonOps/Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07_CommonOps
+{
+    public class Operations
+    {
+        public static int IntOku(string aciklama="Sayı giriniz.")
+        {
+            while (true)
+            {
+                Console.WriteLine(aciklama);
+                string deger = Console.ReadLine();
+                int result = 0;
+                if (int.TryParse(deger, out result))
+                {
+                    return result;
+                }
+            }
+        }
+    }
+}
